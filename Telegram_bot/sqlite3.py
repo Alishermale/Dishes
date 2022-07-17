82a3b8f90fed00ace14ebd4b2ce4c795dc954aef
Alishermale <alishermale@yandex.ru>
2022-07-17 18:01:54 +0300

    sql

---
+++ Telegram_bot/sqlite3.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn = sqlite3.connect('dishes')
+c = conn.cursor()
+
+c.execute('''
+          CREATE TABLE IF NOT EXISTS dishes
+          ([dish_id] INTEGER PRIMARY KEY, [dish_name] TEXT,
+          [description] TEXT, [ingredient] TEXT, [picture])
+          ''')
+
+c.execute('''
+          CREATE TABLE IF NOT EXISTS users
+          ([user_id] INTEGER PRIMARY KEY)
+          ''')
+
+conn.commit()

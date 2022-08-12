4c83dc276e236bfa618f8c24d1184a27d6a13a64
Alishermale <alishermale@yandex.ru>
2022-08-12 20:47:48 +0300

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-import os
+if __name__ == '__main__':
+    from aiogram import executor
+    from bot.handlers import dp
 
-
-os.system('bot/handlers.py')
+    executor.start_polling(dp, skip_updates=True)

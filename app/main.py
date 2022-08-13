cefa5f971674b63fdeeafa3751cad305304d0689
Alishermale <alishermale@yandex.ru>
2022-08-13 17:18:21 +0300

    notify admins on startup

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
+async def on_startup(dp):
+    from app.bot.utils.startup_notify_admins import on_startup_notify
+    await on_startup_notify(dp)
+
+
 if __name__ == '__main__':
     from aiogram import executor
     from bot.handlers import dp
 
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)

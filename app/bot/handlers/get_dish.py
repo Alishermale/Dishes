db12eac40b13820a3d33f695150f7e30a6317bb5
Alishermale <alishermale@yandex.ru>
2022-08-14 12:56:33 +0300

    admins get full message

--- app/bot/handlers/get_dish.py
+++ app/bot/handlers/get_dish.py
@@ -1,6 +1,7 @@
+import os
 import sqlite3
 from aiogram.types import CallbackQuery
-
+from dotenv import load_dotenv
 from app.bot.keyboard.inline import under_dish_buttons
 from app.bot.keyboard.inline.callback_dishes_type import dishes_type_callback
 from app.bot.keyboard.inline.callback_under_dish import under_dish_callback
@@ -10,22 +11,40 @@ from app.loader import dp
 conn = sqlite3.connect('/home/alishermale/Python/My_projects/dishes/app/db/dishes', check_same_thread=False)
 c = conn.cursor()
 last_dish_type = ""
+load_dotenv()
+admins = os.getenv("ADMIN_ID")
 
 
 @dp.callback_query_handler(under_dish_callback.filter(step="next"))
 async def next_button(call: CallbackQuery):
-    d_mess = c.execute(QUERIES.get('random_dish').format(
-        '*', last_dish_type)).fetchone()[1:-1]
-    await call.message.answer_photo(d_mess[-1], caption=d_mess[:-1],
+    if call.from_user.id == int(admins):
+        d_mess = c.execute(QUERIES.get('random_dish').format(
+            '*', last_dish_type)).fetchone()
+        photo = d_mess[-2]
+        caption = (d_mess[:-2], d_mess[-1])
+    else:
+        d_mess = c.execute(QUERIES.get('random_dish').format(
+            '*', last_dish_type)).fetchone()[1:-1]
+        photo = d_mess[-1]
+        caption = (d_mess[:-1])
+    await call.message.answer_photo(photo, caption=caption,
                                     reply_markup=under_dish_buttons)
 
 
 async def sql_requests(call: CallbackQuery, dish_type: str):
     global last_dish_type
-    d_mess = c.execute(QUERIES.get('random_dish').format(
-        '*', dish_type)).fetchone()[1:-1]
+    if call.from_user.id == int(admins):
+        d_mess = c.execute(QUERIES.get('random_dish').format(
+            '*', last_dish_type)).fetchone()
+        photo = d_mess[-2]
+        caption = (d_mess[:-2], d_mess[-1])
+    else:
+        d_mess = c.execute(QUERIES.get('random_dish').format(
+            '*', last_dish_type)).fetchone()[1:-1]
+        photo = d_mess[-1]
+        caption = (d_mess[:-1])
     last_dish_type = dish_type
-    await call.message.answer_photo(d_mess[-1], caption=d_mess[:-1],
+    await call.message.answer_photo(photo, caption=caption,
                                     reply_markup=under_dish_buttons)
 
 

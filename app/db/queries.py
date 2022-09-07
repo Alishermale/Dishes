5169e7688fb4242efa8099f946ffc5a1352cfe98
Alishermale <alishermale@yandex.ru>
2022-09-07 15:34:31 +0300

    add db request for creating all tables in db

--- app/db/queries.py
+++ app/db/queries.py
@@ -15,5 +15,42 @@ QUERIES = {
                     'VALUES(?, ?)',
     'random_dish': 'SELECT * FROM dishes WHERE dish_type=(?) ORDER BY random() LIMIT 1',
     'delete_user': 'DELETE FROM users WHERE user_id={}',
-    'update_dish_type': 'UPDATE dishes SET dish_type = (?) WHERE dish_id = (?)'
+    'update_dish_type': 'UPDATE dishes SET dish_type = (?) WHERE dish_id = (?)',
+    'create_db': '''CREATE TABLE user(
+    id INT PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128),
+    telegram_id CHAR(10),
+    last_dish_type VARCHAR(50)
+);
+CREATE TABLE dish(
+    id INT PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(150),
+    description VARCHAR(800),
+    picture CHAR(67),
+    user_id INT FOREIGN KEY REFERENCES user(id)
+);
+CREATE TABLE ingredient(
+    id INT PRIMARY KEY AUTOINCREMENT,
+    ingredient_name VARCHAR(100)
+);
+CREATE TABLE dish_ingredient(
+    id INT PRIMARY KEY AUTOINCREMENT,
+    dish_id FOREIGN KEY REFERENCES dish(id),
+    ingredient_id FOREIGN KEY REFERENCES ingredient(id)
+);
+CREATE TABLE allergy(
+    id INT PRIMARY KEY AUTOINCREMENT,
+    allergy_name VARCHAR(100),
+    ingredient_id INT FOREIGN KEY REFERENCES ingredient(id),
+    user_id INT FOREIGN KEY REFERENCES user(id)
+);
+CREATE TABLE type(
+    id INT PRIMARY KEY AUTOINCREMENT,
+    type VARCHAR(80)
+);
+CREATE TABLE dish_type(
+    id INT PRIMARY KEY AUTOINCREMENT,
+    type_id INT FOREIGN KEY REFERENCES type(id),
+    dish_id INT FOREIGN KEY REFERENCES dish(id)
+);'''
 }

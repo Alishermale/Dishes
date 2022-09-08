d3ed0e5acb95b0a1cd8605b6824df9a0a750bc4a
Alishermale <alishermale@yandex.ru>
2022-09-08 11:32:09 +0300

    update sql queries

--- app/db/queries.py
+++ app/db/queries.py
@@ -1,56 +1,53 @@
 QUERIES = {
-    'create_table_dish': '''
-              CREATE TABLE IF NOT EXISTS dishes
-              ([dish_id] INTEGER PRIMARY KEY AUTOINCREMENT, [dish_name] TEXT,
-              [description] TEXT, [ingredient] TEXT, [picture] TEXT,
-              [dish_type] TEXT)
-              ''',
-    'create_table_users': '''
-              CREATE TABLE IF NOT EXISTS users
-              ([user_id] int NOT NULL,
-              [user_name] varchar(255) NOT NULL,
-              PRIMARY KEY (user_id))
-              ''',
-    'add_new_user': 'INSERT INTO users '
+    'add_user': 'INSERT INTO users '
                     'VALUES(?, ?)',
     'random_dish': 'SELECT * FROM dishes WHERE dish_type=(?) ORDER BY random() LIMIT 1',
-    'delete_user': 'DELETE FROM users WHERE user_id={}',
+    'deactivate_user': 'DELETE FROM users WHERE user_id={}',
     'update_dish_type': 'UPDATE dishes SET dish_type = (?) WHERE dish_id = (?)',
-    'create_db': '''CREATE TABLE user(
-    id INT PRIMARY KEY AUTOINCREMENT,
+    'create_user': '''CREATE TABLE user(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(128),
-    telegram_id CHAR(10),
-    last_dish_type VARCHAR(50)
+    is_active BIT(1) DEFAULT 1 NOT NULL,
+    telegram_id VARCHAR(15) NOT NULL,
+    last_dish_type VARCHAR(50) NOT NULL
+);''',
+    'create_dish': '''CREATE TABLE dish(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(150) NOT NULL,
+    description VARCHAR(800) NOT NULL,
+    picture CHAR(67) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);''',
+    'create_ingredient': '''CREATE TABLE ingredient(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ingredient_name VARCHAR(100) NOT NULL
+);''',
+    'create_dish_ingredient': '''CREATE TABLE dish_ingredient(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    FOREIGN KEY (dish_id) REFERENCES dish(id),
+    FOREIGN KEY (ingredient_id) REFERENCES ingredient(id)
+);''',
+    'create_allergy': '''CREATE TABLE allergy(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    allergy_name VARCHAR(100) DEFAULT NULL,
+    FOREIGN KEY (ingredient_id) REFERENCES ingredient(id),
+    FOREIGN KEY (user_id) REFERENCES user(id)
 );
-CREATE TABLE dish(
-    id INT PRIMARY KEY AUTOINCREMENT,
-    title VARCHAR(150),
-    description VARCHAR(800),
-    picture CHAR(67),
-    user_id INT FOREIGN KEY REFERENCES user(id)
-);
-CREATE TABLE ingredient(
-    id INT PRIMARY KEY AUTOINCREMENT,
-    ingredient_name VARCHAR(100)
-);
-CREATE TABLE dish_ingredient(
-    id INT PRIMARY KEY AUTOINCREMENT,
-    dish_id FOREIGN KEY REFERENCES dish(id),
-    ingredient_id FOREIGN KEY REFERENCES ingredient(id)
-);
-CREATE TABLE allergy(
-    id INT PRIMARY KEY AUTOINCREMENT,
-    allergy_name VARCHAR(100),
-    ingredient_id INT FOREIGN KEY REFERENCES ingredient(id),
-    user_id INT FOREIGN KEY REFERENCES user(id)
-);
-CREATE TABLE type(
-    id INT PRIMARY KEY AUTOINCREMENT,
-    type VARCHAR(80)
-);
-CREATE TABLE dish_type(
-    id INT PRIMARY KEY AUTOINCREMENT,
-    type_id INT FOREIGN KEY REFERENCES type(id),
-    dish_id INT FOREIGN KEY REFERENCES dish(id)
+UPDATE allergy
+SET allergy_name = (SELECT ingredient_name 
+                    FROM ingredient INNER JOIN allergy 
+                    ON ingredient.id = ingredient_id
+                    WHERE ingredient.id = ingredient_id)
+WHERE allergy_name IS NULL;
+''',
+    'create_type': '''CREATE TABLE type(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type VARCHAR(80) NOT NULL
+);''',
+    'create_dish_type': '''CREATE TABLE dish_type(
+    id INTEGER
+     PRIMARY KEY AUTOINCREMENT,
+    FOREIGN KEY (type_id) REFERENCES type(id),
+    FOREIGN KEY (dish_id) REFERENCES dish(id)
 );'''
 }
